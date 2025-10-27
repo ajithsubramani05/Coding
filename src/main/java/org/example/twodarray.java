f1ab4a67ad05ba0e731d66eaa76507f91b5a92a8
ajith <ajithsubramani05@gmail.com>
2025-10-27 15:15:47 +0530

    Coding

---
+++ src/main/java/org/example/twodarray.java
@@ -0,0 +1,16 @@
+package org.example;
+import java.util.*;
+public class twodarray {
+    static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+int m=sc.nextInt();
+int arr[][]=new int[n][m];
+for(int i=0;i<n;i++){
+    for(int j=0;i<m;i++){
+        arr[i][j]=sc.nextInt();
+    }
+}
+
+    }
+}
