ffc6099f84b1f0e093e511750f490d45d3cf250f
ajith <ajithsubramani05@gmail.com>
2025-10-28 09:56:26 +0530

    Coding

--- src/main/java/org/example/sqaurepattern.java
+++ src/main/java/org/example/sqaurepattern.java
@@ -2,8 +2,6 @@ package org.example;
 import java.util.*;
 public class sqaurepattern {
     static void main(String[] args) {
-
-
         Scanner sc = new Scanner(System.in);
         int n = 4;
         for (int i = 0; i <n; i++) {
